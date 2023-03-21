013c377b858052c7922528277b038f5474af93d0
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-03-21 14:33:11 +0100

    Add the new species to the list for tests

--- tests/test_dwc_occurrence.R
+++ tests/test_dwc_occurrence.R
@@ -180,7 +180,9 @@ testthat::test_that("scientificName is never NA and one of the list", {
     "Fallopia  baldschuanica",
     "Azolla filiculoides",
     "Harmonia axyridis",
-    "Pistia statiotes"
+    "Pistia statiotes",
+    "Zizania latifolia",
+    "Saururus cernuus"
   )
   testthat::expect_true(all(!is.na(dwc_occurrence$scientificName)))
   testthat::expect_true(all(dwc_occurrence$scientificName %in% species))
